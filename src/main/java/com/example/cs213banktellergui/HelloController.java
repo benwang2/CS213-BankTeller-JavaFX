39efeef2d1b71873a1daaa858dcb5c41a87ceed1
Benjamin Wang <eb.benwang@gmail.com>
2022-03-13 16:49:07 -0400

    Implemented swapping panes and tests for textOutput.

--- src/main/java/com/example/cs213banktellergui/HelloController.java
+++ src/main/java/com/example/cs213banktellergui/HelloController.java
@@ -2,13 +2,41 @@ package com.example.cs213banktellergui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
+    @FXML
+    private TextField firstName, lastName, dateOfBirth, amount;
+
+    @FXML
+    private ToggleGroup accountType, accountAction, campus;
+
+    @FXML
+    private VBox printPane, accountPane;
+
     @FXML
     private Label textOutput;
 
     @FXML
-    protected void onHelloButtonClick() {
-        textOutput.setText("Welcome to JavaFX Application!");
+    protected void submit() {
+        textOutput.setText(accountType.getSelectedToggle().toString());
+//        textOutput.setText(accountType.getSelectedToggle().toString());
+    }
+
+    private void mutexVisibility(VBox show, VBox hide){
+        show.setVisible(true);
+        hide.setVisible(false);
+    }
+
+    @FXML
+    protected void displayAccountPane(){
+        mutexVisibility(accountPane, printPane);
+    }
+
+    @FXML
+    protected void displayPrintPane(){
+        mutexVisibility(printPane, accountPane);
     }
 }
