c99bbf28fbfb12d21a8607ba5d5fd7cda8b2ca0c
akash121801 <ass209@scarletmail.rutgers.edu>
2022-03-17 23:03:19 -0400

    Fixed opening command

--- src/main/java/com/example/cs213banktellergui/HelloController.java
+++ src/main/java/com/example/cs213banktellergui/HelloController.java
@@ -28,11 +28,16 @@ public class HelloController {
     @FXML
     private Label textOutput;
 
+
+    /**
+     * A helper function that runs
+     */
     @FXML
     protected void submit(){
         RadioButton selectedAccountActionButton = (RadioButton) accountAction.getSelectedToggle();
         if(selectedAccountActionButton.getText().equals("Open")){
-            textOutput.setText(checkOpeningAccount());
+            textOutput.setText(checkValidAccount());
+
         }
         else if(selectedAccountActionButton.getText().equals("Close")){
 
@@ -43,39 +48,63 @@ public class HelloController {
 
 
 
-    private String checkOpeningAccount(){
-        Account account;
-        if(firstName.getText().trim().isEmpty() || lastName.getText().trim().isEmpty()
+    private String checkValidAccount() {
+        Account account = null;
+        if (firstName.getText().trim().isEmpty() || lastName.getText().trim().isEmpty()
                 || dateOfBirth.getText().trim().isEmpty()
-                || amount.getText().trim().isEmpty()){
+                || amount.getText().trim().isEmpty()) {
             return "Missing data for opening an account.";
-        }
-        else{
+        } else {
             Profile profile = new Profile(firstName.getText(), lastName.getText(), dateOfBirth.getText());
-             if(!profile.isValid()) return "Date of birth invalid.";
-             int balance = Integer.parseInt(amount.getText());
-             if(balance <= 0){
-                 return ("Initial deposit cannot be 0 or negative.");
-             }
-        RadioButton selectedAccountType = (RadioButton) accountType.getSelectedToggle();
-        if(selectedAccountType.getText().equals("Money Market")) {
-            account = new MoneyMarket(profile, balance);
-            if (balance < 2500) { // Need to change this to mm.MIN_BALANCE
-                return "Minimum of $2500 to open a MoneyMarket account.";
+            if (!profile.isValid()) return "Date of birth invalid.";
+            int balance;
+            try {
+                Integer.parseInt(amount.getText());
+            } catch (NumberFormatException e) {
+                return "Not a valid amount.";
             }
+            balance = Integer.parseInt(amount.getText());
+            RadioButton selectedAccountType = (RadioButton) accountType.getSelectedToggle();
+            if (selectedAccountType.getText().equals("Money Market")) {
+                account = new MoneyMarket(profile, balance);
+            } else if (selectedAccountType.getText().equals("College Checking")) {
+                if (campus.getSelectedToggle() == null) return "Invalid Campus Code.";
+                RadioButton selectedCampusButton = (RadioButton) accountAction.getSelectedToggle();
+                account = new CollegeChecking(profile, balance, Integer.parseInt(selectedCampusButton.getText()));
+            } else if (selectedAccountType.getText().equals("Savings")) {
+                RadioButton selectedButton = (RadioButton) accountAction.getSelectedToggle();
+            }
+            account.deposit(balance);
+            RadioButton selectedAccountActionButton = (RadioButton) accountAction.getSelectedToggle();
+            if(selectedAccountActionButton.getText().equals("Open")) return checkOpenAccount(account);
+            else return "";
+
         }
-        else if (selectedAccountType.getText().equals("College Checking")){
-            if(campus.getSelectedToggle() == null) return "Invalid Campus Code.";
-            }
-
-        else if (selectedAccountType.getText().equals("Savings")){
-            RadioButton selectedButton =  (RadioButton) accountAction.getSelectedToggle();
-
-
-            }
-        }
-        return "Account Opened.";
     }
+    /**
+     * A helper function to handle the open command.
+     * @param account An account object to open.
+     * @return A string describing whether the account was already opened, or not enough money to open a MoneyMarket account
+     */
+
+    private String checkOpenAccount(Account account){
+        boolean accountExists = database.doesAccountExist(account);
+
+        if (!accountExists && account.getBalance() <= 0)
+            return("Initial deposit cannot be 0 or negative.");
+
+        if (account instanceof MoneyMarket) {
+            MoneyMarket mm = (MoneyMarket) account;
+            if (mm.getBalance() < mm.MIN_BALANCE)
+                return("Minimum of $2500 to open a MoneyMarket account.");
+        }
+
+        if (database.open(account)) {
+            return accountExists ? "Account reopened." : "Account opened.";
+        } else
+            return account.getHolder().toString()+" same account(type) is in the database.";
+    }
+
 
     private void mutexVisibility(VBox show, VBox hide){
         show.setVisible(true);
